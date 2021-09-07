375a2960eb96cadf026ca8a41c0aa5e7ccf54702
Alexandru-Paul Copil <alexandru@cpl.li>
2021-09-07 19:22:24 +0300

    Optimize transliteration by removing []rune() conversion, replacing strings.Builder with bytes.Buffer with pooled memory

--- transliterator.go
+++ transliterator.go
@@ -1,7 +1,8 @@
 package transliterator
 
 import (
-	"strings"
+	"bytes"
+	"sync"
 	"unicode"
 
 	"github.com/alexsergivan/transliterator/data"
@@ -27,21 +28,32 @@ func NewTransliterator(customLanguageOverrides *map[string]map[rune]string) *Tra
 	}
 }
 
+var memoryPool = sync.Pool{
+	New: func() interface{} {
+		m := make([]byte, 1024)
+		return &m
+	},
+}
+
 // Transliterate performs transliteration of the input text. If the langcode (ISO 639-1) is specified, it will use
 // specific language transliteration rules.
 func (t *Transliterator) Transliterate(text, langcode string) string {
-	var replacement strings.Builder
-	runes := []rune(text)
-	for _, rune := range runes {
+	memory := memoryPool.Get().(*[]byte)
+	defer memoryPool.Put(memory)
+	buffer := bytes.NewBuffer(*memory)
+	buffer.Reset()
+
+	for _, char := range text {
 		if overrides, ok := t.LanguageOverrides.Overrides[langcode]; ok {
-			if val, ok := overrides[rune]; ok {
-				replacement.WriteString(val)
+			if val, ok := overrides[char]; ok {
+				buffer.WriteString(val)
 				continue
 			}
 		}
-		// If the rune number less then maximum ASCII value, use it directly.
-		if rune < unicode.MaxASCII {
-			replacement.WriteString(string(rune))
+
+		// If the char number less then maximum ASCII value, use it directly.
+		if char < unicode.MaxASCII {
+			buffer.WriteRune(char)
 			continue
 		}
 
@@ -49,16 +61,17 @@ func (t *Transliterator) Transliterate(text, langcode string) string {
 		// bank: 0x4
 		// code: 0x7
 
-		// Shifting rune to the right by 8 bits.
-		bank := rune >> 8
+		// Shifting char to the right by 8 bits.
+		bank := char >> 8
+
 		// masks the variable so it leaves only the value in the last 8 bits, and ignores all the rest of the bits
-		code := rune & 0xff
+		code := char & 0xff
 		if transliterationDataVal, ok := t.Data[bank]; ok {
 			if len(transliterationDataVal) > int(code) {
-				replacement.WriteString(transliterationDataVal[code])
+				buffer.WriteString(transliterationDataVal[code])
 			}
 		}
 	}
 
-	return replacement.String()
+	return buffer.String()
 }
