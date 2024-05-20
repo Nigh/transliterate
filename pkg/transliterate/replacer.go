9f278b3db63597a61e9f99a24741e8ba5a08b0c4
xianii <xianyi.xia@outlook.com>
2024-05-21 00:41:34 +0800

    add Separator

--- pkg/transliterate/replacer.go
+++ pkg/transliterate/replacer.go
@@ -13,14 +13,19 @@ import (
 // TODO add comments to exposed types, functions, vars, etc
 
 type Replacer struct {
-	Lang transliterateLang.LangOverwrite
-	Data map[rune][]string
+	Lang      transliterateLang.LangOverwrite
+	Data      map[rune][]string
+	Separator string
 }
 
 var bufferPool = sync.Pool{
 	New: func() interface{} {
 		return bytes.NewBuffer(nil)
 	},
+}
+
+func (replacer *Replacer) WordSeparator(s string) {
+	replacer.Separator = s
 }
 
 // Transliterate performs transliteration of the input text. If the lang (ISO 639-1) is specified, it will use specific
@@ -30,10 +35,15 @@ func (replacer *Replacer) Transliterate(text, lang string) string {
 	defer bufferPool.Put(buffer)
 	buffer.Reset()
 
+	lastType := 0
 	changed := false
 	langOverwrite, hasLangOverwrite := replacer.Lang[lang]
 	for _, char := range text {
 		if char < unicode.MaxASCII {
+			if lastType == 2 {
+				buffer.WriteString(replacer.Separator)
+			}
+			lastType = 1
 			buffer.WriteRune(char)
 			continue
 		}
@@ -42,6 +52,8 @@ func (replacer *Replacer) Transliterate(text, lang string) string {
 
 		if hasLangOverwrite {
 			if value, ok := langOverwrite[char]; ok {
+				buffer.WriteString(replacer.Separator)
+				lastType = 2
 				buffer.WriteString(value)
 				continue
 			}
@@ -51,6 +63,8 @@ func (replacer *Replacer) Transliterate(text, lang string) string {
 		code := char & 0xFF
 
 		if value, ok := replacer.Data[bank]; ok {
+			buffer.WriteString(replacer.Separator)
+			lastType = 2
 			if len(value) > int(code) {
 				buffer.WriteString(value[code])
 			}
